00af61d4cf443c660ee708720ba65014f29a4839
titho93 <titho93.10@gmail.com>
2023-11-23 16:40:08 +0100

    [add]home and main

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1 class="text-center p-5 ">Home</h1>
+    <p class="text-center">This is a homepage of the Comics DC Universe, Good Time! </p>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Comics List</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">New Comics</a>
+        </li>
+    </ul>
+</header>
