5ca7af744a17d7943effe094333bbbc0a126301a
titho93 <titho93.10@gmail.com>
2023-11-24 00:42:06 +0100

    [edit]form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -25,7 +25,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="price" class="form-label">Price</label>
-                    <input type="text" class="form-control" id="cooking_time" name="cooking_time">
+                    <input type="text" class="form-control" id="price" name="price">
                 </div>
                 <div class="mb-3">
                     <label for="sale_date" class="form-label">Sale Date</label>
