6fb3364f9804b68487a843a6a7f3e6e8f437b5b8
titho93 <titho93.10@gmail.com>
2023-11-23 23:21:40 +0100

    [edit]route web

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'index'])->name('home');
+
+Route::resource('comics', ComicController::class);
