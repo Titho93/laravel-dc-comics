3ee87643b00b4fd39fd02bc21daf8b8b34e31cf0
titho93 <titho93.10@gmail.com>
2023-11-23 16:26:52 +0100

    [add]model and controller

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
 
 class ComicsTableSeeder extends Seeder
 {
@@ -14,6 +16,6 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
     }
 }
