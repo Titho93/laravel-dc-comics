1895b24276ac9f5b4f57b1e857ae5d8a567b3936
titho93 <titho93.10@gmail.com>
2023-11-23 23:26:59 +0100

    [edit]table index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Comics list</h1>
+    <h1 class="p-5">Comics list</h1>
     <table class="table">
         <thead>
             <tr>
                 <th scope="col">ID</th>
                 <th scope="col">Title</th>
+                <th scope="col">Price</th>
+                <th scope="col">Series</th>
+                <th scope="col">Sale Date</th>
+                <th scope="col">Type</th>
             </tr>
         </thead>
         <tbody>
@@ -14,6 +18,11 @@
                 <tr>
                     <td>{{ $comic->id }}</td>
                     <td>{{ $comic->title }}</td>
+                    <td>{{ $comic->price }}</td>
+                    <td>{{ $comic->series }}</td>
+                    <td>{{ $comic->sale_date }}</td>
+                    <td>{{ $comic->type }}</td>
+
                 </tr>
             @endforeach
 
