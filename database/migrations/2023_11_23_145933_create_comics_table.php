9ed163bf2bb166a2e3ca04d03d6bf2295e65c68a
titho93 <titho93.10@gmail.com>
2023-11-23 16:17:59 +0100

    [add]migration

--- database/migrations/2023_11_23_145933_create_comics_table.php
+++ database/migrations/2023_11_23_145933_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumb');
+            $table->string('price', 15);
+            $table->string('series', 20);
+            $table->date('sale_date')->nullable();
+            $table->string('type', 20);
+            $table->string('artist');
+            $table->string('writers');
             $table->timestamps();
         });
     }
